36cbeffe67f8b78545982106f33919b6ab99597f
skodie <samuelmkodie@gmail.com>
2023-12-08 22:18:12 +0000

    template added

---
+++ technocrats-website/src/pages/RoundRockBicycleRepair.js
@@ -0,0 +1,151 @@
+import "./RoundRockBicycleRepair.css";
+
+const RoundRockBicycleRepair = () => {
+  return (
+    <div className="round-rock-bicycle-repair">
+      <div className="group-parent">
+        <img className="frame-child" alt="" src="/group-15.svg" />
+        <div className="frame-item" />
+        <img className="frame-inner" alt="" src="/ellipse-2@2x.png" />
+        <div className="what-is-lorem-ipsum-what-is-lo-parent">
+          <div className="what-is-lorem-container">
+            <p className="what-is-lorem">What is Lorem Ipsum</p>
+            <p className="what-is-lorem-ipsum">
+              <b className="what-is-lorem1">What is Lorem Ipsum</b>
+            </p>
+          </div>
+          <div className="lorem-ipsum-is">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </div>
+          <div className="rectangle-parent">
+            <div className="group-child" />
+            <b className="help-me">Help Me</b>
+          </div>
+        </div>
+        <img className="group-icon" alt="" src="/group-558.svg" />
+      </div>
+      <div className="rectangle-group">
+        <div className="rectangle-div" />
+        <div className="rectangle-container">
+          <div className="group-item" />
+          <b className="get-discount-10">Get discount 10%</b>
+        </div>
+        <div className="ellipse-parent">
+          <div className="group-inner" />
+          <div className="lorem-ipsum-is1">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text\
+          </div>
+          <b className="diagnosis-and-adjustment">
+            Diagnosis and adjustment of the bike
+          </b>
+          <img className="ellipse-icon" alt="" src="/ellipse-4@2x.png" />
+        </div>
+        <div className="ellipse-group">
+          <div className="group-inner" />
+          <div className="lorem-ipsum-is1">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+          </div>
+          <b className="diagnosis-and-adjustment">
+            Diagnosis and adjustment of the bike
+          </b>
+          <img className="ellipse-icon" alt="" src="/ellipse-41@2x.png" />
+        </div>
+        <div className="ellipse-container">
+          <div className="group-inner" />
+          <div className="lorem-ipsum-is1">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+          </div>
+          <b className="diagnosis-and-adjustment">
+            Diagnosis and adjustment of the bike
+          </b>
+          <img className="ellipse-icon" alt="" src="/ellipse-5@2x.png" />
+        </div>
+        <div className="group-div">
+          <div className="group-inner" />
+          <div className="lorem-ipsum-is1">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+          </div>
+          <b className="diagnosis-and-adjustment">
+            Diagnosis and adjustment of the bike
+          </b>
+          <img className="ellipse-icon" alt="" src="/ellipse-6@2x.png" />
+        </div>
+      </div>
+      <div className="rectangle-parent1">
+        <div className="group-child6" />
+        <div className="repair-and-maintenance">Repair and maintenance</div>
+        <b className="services-for-you">Services for you</b>
+      </div>
+      <div className="frame-div">
+        <div className="frame-child1" />
+        <img className="mask-group-icon" alt="" src="/mask-group.svg" />
+        <div className="we-are-a-big-family-parent">
+          <div className="we-are-a">We are a big family</div>
+          <b className="about-us">About Us</b>
+        </div>
+        <div className="frame-child2" />
+        <img className="rectangle-icon" alt="" src="/rectangle-7@2x.png" />
+        <div className="group-container">
+          <div className="rectangle-parent2">
+            <div className="group-item" />
+            <b className="get-discount-10">Get discount 10%</b>
+          </div>
+          <div className="lorem-ipsum-is5">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </div>
+          <div className="company-name">Company name</div>
+          <div className="group-child8" />
+        </div>
+      </div>
+      <div className="round-rock-bicycle-repair-child" />
+      <img className="mask-group-icon1" alt="" src="/mask-group1.svg" />
+      <div className="round-rock-bicycle-repair-item" />
+      <div className="leave-a-request-parent">
+        <div className="leave-a-request">Leave a request</div>
+        <b className="about-us">Contact Us</b>
+      </div>
+      <div className="rectangle-parent3">
+        <div className="group-child9" />
+        <div className="group-child10" />
+        <div className="group-child11" />
+        <div className="group-child12" />
+      </div>
+      <div className="round-rock-bicycle-repair-inner" />
+      <div className="testgmailcom-parent">
+        <div className="testgmailcom">test@gmail.com</div>
+        <img className="icround-mail-icon" alt="" src="/icroundmail.svg" />
+      </div>
+      <div className="parent">
+        <div className="testgmailcom">(303) 555-0105</div>
+        <img className="icround-mail-icon" alt="" src="/icroundcall.svg" />
+      </div>
+      <div className="ash-dr-san-jose-south-dakota-parent">
+        <div className="testgmailcom">
+          2715 Ash Dr. San Jose, South Dakota 83475
+        </div>
+        <img
+          className="icround-mail-icon"
+          alt=""
+          src="/icroundlocationon.svg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RoundRockBicycleRepair;
