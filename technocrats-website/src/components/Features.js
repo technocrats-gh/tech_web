57db5f4475e440191767b9bb3c25a792fc8143c0
skodie <samuelmkodie@gmail.com>
2023-12-19 16:05:48 +0000

    altered header, hero and service components on top of new responsive UI

---
+++ technocrats-website/src/components/Features.js
@@ -0,0 +1,41 @@
+import {
+  CursorClickIcon,
+  HeartIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
+import React from "react";
+import Feature from "./FeatureItem";
+
+function Features() {
+  return (
+    <section id="services" className="container mt-24 flex flex-col items-center">
+      <h2 className="text-[32px] font-bold text-center sm:text-left">
+        What We Specialize In
+      </h2>
+
+      <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-3 gap-12 items-center">
+
+      <Feature
+        Icon={CursorClickIcon}
+        title="Web & App Development"
+        iconBgColor="#02897A"
+        description="We create websites and web applications that are accessible via the internet to solve everyday problems facing various industries and personal needs"
+      />
+      <Feature
+        Icon={UsersIcon}
+        iconBgColor="#4D8DFF"
+        title="Data Analysis & Machine Learning"
+        description="We use several iterative phases, including identifying the business question, collecting raw data sets, cleaning the data, analyzing the data, and interpreting the results of the analysis"
+      />
+      <Feature
+        Icon={HeartIcon}
+        iconBgColor="#740A76"
+        title="Mechatronics"
+        description="we implore  a combination of robotics, computer science, telecommunications, systems, control, and product engineering to help create solutions for our customers"
+      />
+      </div>
+    </section>
+  );
+}
+
+export default Features;
---
+++ technocrats-website/src/components/Hero.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Robot from '../assets/images/RobortLG.png';
+import "../styles/hero.css"
+
+function Hero() {
+  return (
+    // parent div
+    <>
+      <div className="hidden md:hidden rectangles lg:flex">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <section id="home" className="container mt-14 md:flex flex-row justify-between items-center w-full">
+        <div className="md:max-w-[50%] lg:max-w-[62%] mt-14 mr-12 mb-8 z-30">
+          <img src={Robot} alt="hero" />
+        </div>
+
+        {/* text section */}
+        <div className="text-center sm:text-left md:max-w-[40%] z-30">
+          <h1 className="font-bold text-4xl leading-[60px]">
+            Meet the Trailblazers: Young Minds Shaping the Future
+          </h1>
+          <p className="mt-4 text-[18px] leading-[28px] font-normal italic wrapper p-2 rounded-2xl">
+            Welcome to a world where innovation knows no bounds, and the future is forged by the hands of the young and the ambitious.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
