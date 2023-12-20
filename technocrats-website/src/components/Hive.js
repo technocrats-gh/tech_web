3b60b80b8324bcbc04b872d24e409f7f365f8786
new2me321 <kwasiboatjr@live.co.uk>
2023-12-20 23:12:33 +0200

    team changes

--- technocrats-website/src/components/Hive.js
+++ technocrats-website/src/components/Hive.js
@@ -2,9 +2,11 @@ import React from "react";
 import Image1 from "../assets/images/team/amankwah.jpg";
 import Image2 from "../assets/images/team/asare.jpg";
 import Image3 from "../assets/images/team/botchway.jpg";
-import Image4 from "../assets/images/team/damste.jpg";
+import Image4 from "../assets/images/team/damtse.jpg";
 import Image5 from "../assets/images/team/gyimah.jpg";
 import Image6 from "../assets/images/team/kodie.jpg";
+import Image7 from "../assets/images/team/faako.jpg";
+import Image8 from "../assets/images/team/boateng.jpg";
 import "../styles/hive.css";
 
 function Hive() {
@@ -29,8 +31,12 @@ function Hive() {
                 <div class="hive_grid_item">
                     <img src={Image6} alt="Team Member"/>
                 </div>
-                <div class="hive_grid_item"></div>
-                <div class="hive_grid_item"></div>
+                <div class="hive_grid_item">
+                    <img src={Image7} alt="Team Member" />
+                </div>
+                <div class="hive_grid_item">
+                    <img src={Image8} alt="Team Member" />
+                </div>
             </div>
         </div>
     )
