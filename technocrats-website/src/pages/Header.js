90558f3c0c98972cade8a7981820376a56d9af90
skodie <samuelmkodie@gmail.com>
2024-01-05 11:39:50 +0000

    changed favicon and fixed header and drawer nav

--- technocrats-website/src/pages/Header.js
+++ technocrats-website/src/pages/Header.js
@@ -1,68 +1,71 @@
 import React from "react";
-import LogoWhite from "../assets/images/technocrats-logos/technocrats-logos_stroke.png"
+import LogoWhite from "../assets/images/technocrats-logos/technocrats-logos_stroke.png";
+import LogoShort from "../assets/images/technocrats-logos/technocrats-logos_crop.jpeg";
+import { Facebook, LinkedIn, GitHub } from "../components/Icons";
 import "../index.css";
 
 function Header({ setIsOpen }) {
   const navItems = [
-    { label: 'Services', nav: "services" },
-    { label: 'About Us', nav: 'about' },
+    { label: 'What We Specialize In', nav: "services" },
+    { label: 'Our Experts', nav: 'team' },
+    { label: "What Clients Say", nav: "testimonials" },
+    //change nav to footer id when merged with Amoako's changes
+    { label: "Contact Us", nav: "testimonials" }
   ];
 
+  const handleClickScroll = (path) => {
+    const element = document.getElementById(path);
+    const headerOffset = 150;
+    const elementPosition = element.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth"
+    });
+  };
+
   return (
-    <header className="container flex shadow-md md:shadow-none h-30">
+    <header className="flex shadow-md md:shadow-none h-30">
       {/** Nav bar for large screens */}
-      <div className=" hidden md:hidden lg:flex justify-between w-full mt-5 items-center z-40 bg-zinc-900 p-6 mb-5 rounded-xl">
-        <div className="">
+      <div className="hidden md:hidden lg:flex w-11/12 mt-5 items-center z-40 bg-zinc-800 p-6 mb-5 rounded-xl fixed ml-14">
+        <div className="flex flex-row">
           {navItems.map(navItem => (
-            <a href={navItem.nav} className="lg:nav-item">{navItem.label}</a>
+            <div onClick={() => handleClickScroll(navItem.nav)} className="lg:nav-item">{navItem.label}</div>
           ))}
         </div>
 
-        <a href="home">
+        <div className="flex justify-between items-center ml-10 w-7/12">
+          <div className="cursor-pointer" onClick={() => handleClickScroll("home")}>
           <img
             className="h-11"
             src={LogoWhite}
             alt=""
           />
-        </a>
+          </div>
 
-        <div className="flex space-x-4">
-          <svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 448 512">
-            <path fill="#cfb214" d="M448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 
-            64-64V96zM265.8 407.7c0-1.8 0-6 .1-11.6c.1-11.4 .1-28.8 .1-43.7c0-15.6-5.2-25.5-11.3-30.7c37-4.1 76-9.2 
-            76-73.1c0-18.2-6.5-27.3-17.1-39c1.7-4.3 7.4-22-1.7-45c-13.9-4.3-45.7 17.9-45.7 17.9c-13.2-3.7-27.5-5.6-41.6-5.6s-28.4 1.9-41.6 
-            5.6c0 0-31.8-22.2-45.7-17.9c-9.1 22.9-3.5 40.6-1.7 45c-10.6 11.7-15.6 20.8-15.6 39c0 63.6 37.3 69 74.3 73.1c-4.8 4.3-9.1 11.7-10.6 
-            22.3c-9.5 4.3-33.8 11.7-48.3-13.9c-9.1-15.8-25.5-17.1-25.5-17.1c-16.2-.2-1.1 10.2-1.1 10.2c10.8 5 18.4 24.2 18.4 24.2c9.7 29.7 56.1 
-            19.7 56.1 19.7c0 9 .1 21.7 .1 30.6c0 4.8 .1 8.6 .1 10c0 4.3-3 9.5-11.5 8C106 393.6 59.8 330.8 59.8 257.4c0-91.8 70.2-161.5 162-161.5s166.2 
-            69.7 166.2 161.5c.1 73.4-44.7 136.3-110.7 158.3c-8.4 1.5-11.5-3.7-11.5-8zm-90.5-54.8c-.2-1.5 1.1-2.8 3-3.2c1.9-.2 3.7 
-            .6 3.9 1.9c.3 1.3-1 2.6-3 3c-1.9 .4-3.7-.4-3.9-1.7zm-9.1 3.2c-2.2 .2-3.7-.9-3.7-2.4c0-1.3 1.5-2.4 3.5-2.4c1.9-.2 3.7 
-            .9 3.7 2.4c0 1.3-1.5 2.4-3.5 2.4zm-14.3-2.2c-1.9-.4-3.2-1.9-2.8-3.2s2.4-1.9 4.1-1.5c2 .6 3.3 2.1 2.8 3.4c-.4 1.3-2.4 1.9-4.1 
-            1.3zm-12.5-7.3c-1.5-1.3-1.9-3.2-.9-4.1c.9-1.1 2.8-.9 4.3 .6c1.3 1.3 1.8 3.3 .9 4.1c-.9 1.1-2.8 .9-4.3-.6zm-8.5-10c-1.1-1.5-1.1-3.2 0-3.9c1.1-.9 
-            2.8-.2 3.7 1.3c1.1 1.5 1.1 3.3 0 4.1c-.9 .6-2.6 0-3.7-1.5zm-6.3-8.8c-1.1-1.3-1.3-2.8-.4-3.5c.9-.9 2.4-.4 3.5 .6c1.1 1.3 1.3 2.8 .4 3.5c-.9 .9-2.4 
-            .4-3.5-.6zm-6-6.4c-1.3-.6-1.9-1.7-1.5-2.6c.4-.6 1.5-.9 2.8-.4c1.3 .7 1.9 1.8 1.5 2.6c-.4 .9-1.7 1.1-2.8 .4z"/>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 512 512">
-            <path fill="#cfb214" d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 
-            125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 448 512">
-            <path fill="#cfb214" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 
-           416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 
-           243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
-          </svg>
+          <div className="flex space-x-4">
+            <GitHub />
+            <Facebook />
+            <LinkedIn />
+          </div>
         </div>
       </div>
 
       {/** Nav bar for mobile screens */}
-      <div className="flex lg:hidden justify-between items-center pt-3">
+      <div className="flex lg:hidden justify-between items-center pt-3 ml-4 w-11/12 fixed bg-zinc-900 z-40">
         <img
-          className="lg:hidden"
-          src={LogoWhite}
+          className="lg:hidden rounded-3xl m-2"
+          src={LogoShort}
           alt=""
-          width="150"
+          width="45"
         />
         {/** onClick to open nav drawer */}
-        <i className="fa-solid fa-bars h-10 lg:hidden" onClick={() => setIsOpen(true)}></i>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6" onClick={() => setIsOpen(true)} className="h-12">
+          <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 
+          .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd"
+          />
+        </svg>
       </div>
     </header>
   );
